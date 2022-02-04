2eafe96b528d1f73e844ecf5baf6185b1dbfe1d3
Eduard Romaniuk <eduard.romanyuk1990@gmail.com>
2022-02-04 12:39:14 +0200

    hw3 nginx ingress controller metrics

--- hw3/destroy.sh
+++ hw3/destroy.sh
@@ -2,4 +2,5 @@
 helm delete hw3-service --namespace otus-lab
 helm delete postgres-lab --namespace otus-lab
 helm delete prometheus --namespace otus-lab
+helm delete nginx-ingress --namespace otus-lab
 kubectl delete pvc data-postgres-lab-postgresql-0 --namespace otus-lab
--- hw3/init.sh
+++ hw3/init.sh
@@ -2,9 +2,8 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
+helm install nginx-ingress --namespace otus-lab bitnami/nginx-ingress-controller -f helm-configs/nginx-ingress.yml
 helm install hw3-service --namespace otus-lab ./service-chart --set db.password=test-password
 helm install postgres-lab --namespace otus-lab bitnami/postgresql --set auth.existingSecret=postgres-secret
-helm install prometheus --namespace otus-lab prometheus-community/kube-prometheus-stack \
-  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
-  --set prometheus.prometheusSpec.serviceMonitorSelector={}
+helm install prometheus --namespace otus-lab prometheus-community/kube-prometheus-stack -f helm-configs/prometheus.yml
 
